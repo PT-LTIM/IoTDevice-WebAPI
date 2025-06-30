83c682a22ddd15e8f8203e1f2ea0f94e0ea4b12d
Pratik Nandkishor Thorve <pratik.10836190@ltimindtree.com>
2025-06-30 19:23:02 +0530

    K8sAdded

--- obj/Debug/net8.0/IoTDeviceWebAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/IoTDeviceWebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IoTDeviceWebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+541da1c20fedf38a60af197d0e812756e8cc6121")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e13191e7a92faf6209bf80672c2d617af38779c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("IoTDeviceWebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IoTDeviceWebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
