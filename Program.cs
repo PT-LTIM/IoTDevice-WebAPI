a667d1f0d6fc3f996f51386d2b92d4eb3c3ff40b
Pratik Nandkishor Thorve <pratik.10836190@ltimindtree.com>
2025-07-08 14:25:48 +0530

    isProdAppsettings

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddSingleton<TelemetryService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
